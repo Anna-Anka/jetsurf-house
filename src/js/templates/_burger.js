7add7fe1af97fd1cadcd07f1989d68d2bce41d77
Anna-Anka <anjbekrenewa@gmail.com>
2025-08-18 21:20:36 +0300

    add burger-menu

--- src/js/templates/_burger.js
+++ src/js/templates/_burger.js
@@ -1,6 +1,7 @@
 import { disableScroll, enableScroll } from '../utils/index.js';
 
 (function() {
+    console.log('burger')
     const burgerButton = document.querySelector('[data-burger-button]');
     const menu = document.querySelector('[data-menu]');
     const menuLinks = document.querySelectorAll('[data-menu-link]');
@@ -24,7 +25,7 @@ import { disableScroll, enableScroll } from '../utils/index.js';
     const hideBurger = () => {
         burgerButton.classList.remove('burger-button--active');
         menu.classList.remove('burger-menu--active');
-        overlay.classList.remove('overlay--active');
+        overlay?.classList.remove('overlay--active');
     }
 
     burgerButton.addEventListener('click', () => {
@@ -72,4 +73,4 @@ import { disableScroll, enableScroll } from '../utils/index.js';
             }
         }
     }
-})
+})()
