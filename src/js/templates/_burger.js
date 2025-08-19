d28d37e74a95e7465e3877140a5cbaf1533c2513
Anna-Anka <anjbekrenewa@gmail.com>
2025-08-19 11:15:34 +0300

    fix burger-menu

--- src/js/templates/_burger.js
+++ src/js/templates/_burger.js
@@ -1,11 +1,12 @@
-import { disableScroll, enableScroll } from '../utils/index.js';
+import { disableScroll, enableScroll, globalVars } from '../utils/index.js';
 
 (function() {
-    console.log('burger')
     const burgerButton = document.querySelector('[data-burger-button]');
     const menu = document.querySelector('[data-menu]');
     const menuLinks = document.querySelectorAll('[data-menu-link]');
     const overlay = document.querySelector('[data-menu-overlay]');
+
+    if (!burgerButton || !menu || !menuLinks || !overlay) return;
 
     const checkClass = () => {
         if (burgerButton.classList.contains('burger-button--active')) {
@@ -25,7 +26,7 @@ import { disableScroll, enableScroll } from '../utils/index.js';
     const hideBurger = () => {
         burgerButton.classList.remove('burger-button--active');
         menu.classList.remove('burger-menu--active');
-        overlay?.classList.remove('overlay--active');
+        overlay.classList.remove('overlay--active');
     }
 
     burgerButton.addEventListener('click', () => {
