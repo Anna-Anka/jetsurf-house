b72794abd9cb7ad3d97e366cd17e5c9049b24d19
Anna-Anka <anjbekrenewa@gmail.com>
2025-09-01 18:40:06 +0300

    refactor reviews block

--- src/js/project/_reviews-swiper.js
+++ src/js/project/_reviews-swiper.js
@@ -1,42 +1,37 @@
 if (document.querySelector('.reviews-about__swiper')) {
-  new Swiper('.reviews-about__swiper', {
-    slidesPerView: "auto",
-    centeredSlides: true,
-    loop: true,
-    slidesOffsetBefore: 255,
-    grabCursor: true,
+    new Swiper('.reviews-about__swiper', {
+        slidesPerGroup: 1,
+        loop: true,
+        grabCursor: true,
 
-    navigation: {
-      nextEl: '.reviews-about__arrow--next',
-      prevEl: '.reviews-about__arrow',
-    },
+        navigation: {
+            nextEl: '.reviews-about__arrow--next',
+            prevEl: '.reviews-about__arrow',
+        },
 
+        scrollbar: {
+            el: '.reviews-about__scrollbar',
+            draggable: true,
+        },
 
-    scrollbar: {
-      el: '.reviews-about__scrollbar',
-      draggable: true,
-    },
-
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        slidesOffsetBefore: 0,
-        centeredSlides: true,
-      },
-      560: {
-        slidesPerView: "auto",
-        slidesOffsetBefore: 50,
-      },
-      993: {
-        slidesPerView: "auto",
-        slidesOffsetBefore: 220,
-      },
-      1440: {
-        slidesPerView: "auto",
-        slidesOffsetBefore: 255,
-      }
-    },
-  });
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            560: {
+                slidesPerView: 1.5,
+                spaceBetween: 50,
+            },
+            992: {
+                slidesPerView: 1.8,
+            },
+            1200: {
+                slidesPerView: 2.5,
+                spaceBetween: 60,
+            }
+        },
+    });
 }
 
 
